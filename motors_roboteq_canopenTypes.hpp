96b578a0b29c51f6e59f68794f20fac6f66a0ea0
Sylvain <sylvain.joyeux@tidewise.io>
2019-09-23 18:04:34 -0300

    initial commit by rock-create-orogen

---
+++ motors_roboteq_canopenTypes.hpp
@@ -0,0 +1,15 @@
+#ifndef motors_roboteq_canopen_TYPES_HPP
+#define motors_roboteq_canopen_TYPES_HPP
+
+/* If you need to define types specific to your oroGen components, define them
+ * here. Required headers must be included explicitly
+ *
+ * However, it is common that you will only import types from your library, in
+ * which case you do not need this file
+ */
+
+namespace motors_roboteq_canopen {
+}
+
+#endif
+
